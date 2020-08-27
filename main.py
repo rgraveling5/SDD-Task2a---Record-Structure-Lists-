bda18ed5ad830161f62d55edf541c2bfef8b9fc9
Rachael Graveling <clrg092@cl.glow.scot>
2020-08-27 11:32:55 +0000

    Short way (square brackets). Not working

--- main.py
+++ main.py
@@ -11,19 +11,17 @@ student = []
 
 student.append(input('Please enter name: '))
 
-age = int(input("Please enter age: "))
-while age <1 or age >1500:
-  age = int(input("Please re-enter age: "))
-  print()
-student.append(age)
+student.append(int(input('Please enter age: ')))
+while student[1] <1 or student[1] >150:
+  print ("Error. Must be between 1 and 150.")
+  student.append(int(input('Please enter age: ')))
 
-height = float(input("Please enter height (m): "))
-while height <1 or height > 8.5:
-  height = float(input("Please re-enter height(m): "))
-  print()
-student.append(height)
- 
+student.append(float(input('Please enter height: ')))
+
+while student[2] <1 or student[2] >2.5:
+  print("Error. Must be between 1 and 2.5.")
+  student.append(float(input('Please enter height: ')))
+
 student.append(input('What is your home town?: '))
 
-print()
 print(student)
