dfaa84f50489a310572290329dd19527c5ad512c
Rachael Graveling <clrg092@cl.glow.scot>
2020-09-03 10:20:39 +0000

    Method from onenote

--- main.py
+++ main.py
@@ -4,21 +4,35 @@
 #Rachael Graveling
 #28/08/2020
 
-student = []
+# List to store all records
+student_records = []
 
-student.append(input('Please enter name: '))
+# Number of records
+number_of_students = 3
 
-student.append(int(input('Please enter age: ')))
-while student[1] <1 or student[1] >150:
-  print ("Error. Must be between 1 and 150.")
-  student[1] = int(input('Please enter age: '))
+# Loop for each student
+for x in range(number_of_students):
+    student = []
 
-student.append(float(input('Please enter height: ')))
+    student.append(input('Please enter name: '))
 
-while student[2] <1 or student[2] >2.5:
-  print("Error. Must be between 1 and 2.5.")
-  student[2]= float(input('Please enter height: '))
+    student.append(float(input('Please enter age : ')))
+    while student[1] < 1 or student[1] > 150 or not student[1].is_integer():
+        print('Error. Must be a whole number between 1 & 150')
+        student[1] = float(input('Please enter age : '))
 
-student.append(input('What is your home town?: '))
+    student[1] = int(student[1])
 
-print(student)
+    student.append(float(input('Please enter height : ')))
+    while student[2] < 1 or student[2] > 2.5:
+        print('Error. Must be a number between 1 & 2.5')
+        student[2] = float(input('Please enter height : '))
+
+    student.append(input('What is your home town?: '))
+
+    # Add the student record to list of records
+    student_records.append(student)
+
+# Print the records
+for x in student_records:
+    print(x)
