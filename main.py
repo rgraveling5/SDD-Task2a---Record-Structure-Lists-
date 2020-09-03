8abb13e1e115ced156ebe0902068e7a0419bf325
Rachael Graveling <clrg092@cl.glow.scot>
2020-09-03 10:19:50 +0000

    03/09/2020

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-'''
-Program Title
 
-A Programmer
-01/01/1970
+#Record structure
 
-A brief description of what the program does
-'''
+#Rachael Graveling
+#28/08/2020
 
 student = []
 
@@ -14,13 +11,13 @@ student.append(input('Please enter name: '))
 student.append(int(input('Please enter age: ')))
 while student[1] <1 or student[1] >150:
   print ("Error. Must be between 1 and 150.")
-  student.append(int(input('Please enter age: ')))
+  student[1] = int(input('Please enter age: '))
 
 student.append(float(input('Please enter height: ')))
 
 while student[2] <1 or student[2] >2.5:
   print("Error. Must be between 1 and 2.5.")
-  student.append(float(input('Please enter height: ')))
+  student[2]= float(input('Please enter height: '))
 
 student.append(input('What is your home town?: '))
 
